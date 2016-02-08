300a2406bf7ee7aa3ad3b6e894bcf6297bfd553a
sonoftheweb <prometheus6001@gmail.com>
2016-02-08 14:05:03 +0100

    made modifications. Apparently you cannot have comments in .h files. Also added function to print all alphabetic letters in lowercase.

--- i_m_not_in_the_loop/1-print_alphabet.c
+++ i_m_not_in_the_loop/1-print_alphabet.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "my_functions.h"
 
+//print all alphabets from a to z in lowercase. This is only possible because c treats all
+//as numbers
 void print_alphabet(void){
 	char alphabet;
 	for(alphabet = 'a'; alphabet <= 'z'; alphabet++ ){
